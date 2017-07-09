cf6535c174e9cd740f27c0795fedbd6245bfd283
herereadthis <herereadthis@gmail.com>
2017-07-08 23:55:34 -0400

    some unit tests like for real

--- anyone/anyone.py
+++ anyone/anyone.py
@@ -21,14 +21,25 @@ class Poem:
 
     def get_author(self):
         """Very redundant. You can do instance.author. It's just a demo."""
-        author = self.author
-        return author
+        return self.author
 
-    def get_verse(self, v):
+    def get_publication(self):
+        """Very redundant. You can do instance.publication."""
+        return self.publication
+
+    def get_title(self):
+        """Very redundant. You can do instance.title."""
+        return self.title
+
+    def get_verse(self, v=0):
         """Get a specific verse."""
         verse_count = len(self.verses)
         if v < verse_count:
             return self.verses[v]
+
+    def get_line_in_verse(self, v=0, l=0):
+        """Return a specific line from a specific verse."""
+        return 123
 
     def print_poem(self):
         """Print all the verses."""
--- tests/test_anyone.py
+++ tests/test_anyone.py
@@ -1,23 +1,37 @@
 """Unit tests for anyone."""
 
 import pytest
-import anyone
+from anyone import anyone
 
 
 @pytest.fixture
 def response():
+    """Test fixture."""
     import requests
     return requests.get('https://github.com/herereadthis/anyone.git')
 
-def func(x):
-    return x + 1
 
-def test_answer():
-    bar = anyone.main()
-    la = 'asdf'
-    print(la)
-    assert bar == 'bar'
-    assert func(3) == 4
+def test_author():
+    """Test getting author."""
+    poem = anyone.Poem()
+    assert poem.author == poem.get_author()
 
-def test_content(response):
-    assert func(3) == 4
+
+def test_publication():
+    """Test getting publication."""
+    poem = anyone.Poem()
+    assert poem.publication == poem.get_publication()
+
+
+def test_title():
+    """Test getting title."""
+    poem = anyone.Poem()
+    assert poem.title == poem.get_title()
+
+
+def test_verse():
+    """Test getting specific verse."""
+    poem = anyone.Poem()
+    assert poem.get_verse() == poem.verses[0]
+    assert poem.get_verse(1) == poem.verses[1]
+    assert poem.publication == poem.get_publication()
